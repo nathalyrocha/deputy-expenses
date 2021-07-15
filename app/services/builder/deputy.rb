088f83634348dc9735f6a08da7b3ee82cc932ff0
Náthaly Rocha <nathalyfroliveira@gmail.com>
2021-07-15 15:01:03 -0300

    Add deputy builder

---
+++ app/services/builder/deputy.rb
@@ -0,0 +1,18 @@
+class Builder::Deputy
+  attr_reader :line, :parser
+
+  def initialize(line, party, parser = Parser::Deputy)
+    @line = line
+    @party = party
+    @parser = parser.new(line)
+  end
+
+  def call
+    ::Deputy.find_or_create_by(
+      name: parser.name, 
+      cpf: parser.cpf, 
+      party: party,
+      state: parser.state 
+    )
+  end
+end
