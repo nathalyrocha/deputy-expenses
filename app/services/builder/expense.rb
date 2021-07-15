93ed2bd1e276f2b751e446e0a1894d7d671b1f21
Náthaly Rocha <nathalyfroliveira@gmail.com>
2021-07-15 15:01:15 -0300

    Add expense builder

---
+++ app/services/builder/expense.rb
@@ -0,0 +1,41 @@
+class Builder::Expense
+  attr_reader :line, :parser
+
+  def initialize(line, parser = Parser::Expense)
+    @line = line
+    @parser = parser.new(line)
+  end
+
+  def save
+    return unless valid?
+    expense
+  end
+
+  private
+
+  def valid?
+    deputy.valid? && provider.valid?
+  end
+
+  def party
+    @party ||= Builder::Party.new(line).call
+  end
+
+  def provider
+    @provider = Builder::Provider.new(line).call
+  end
+
+  def deputy
+    @deputy = Builder::Deputy.new(line, party).call
+  end
+
+  def expense
+    ::Expense.find_or_create_by(
+      net_value: parser.net_value, 
+      emission_date: parser.emission_date, 
+      description: parser.description, 
+      provider: provider, 
+      deputy: deputy
+    )
+  end
+end
