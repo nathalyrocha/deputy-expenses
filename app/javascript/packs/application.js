76c68bb490e18282bfe0cd15aada1236325301d0
Náthaly Rocha <nathalyfroliveira@gmail.com>
2021-07-16 16:56:03 -0300

    Import chartkick

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,8 @@ import 'bootstrap';
 import 'bootstrap/dist/js/bootstrap';
 import "bootstrap/dist/css/bootstrap";
 
+import "chartkick/chart.js"
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
