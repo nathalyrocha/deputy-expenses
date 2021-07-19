1bb755bb51bda4a69fcae4053dcd6364d53a5de3
Náthaly Rocha <nathalyfroliveira@gmail.com>
2021-07-19 13:49:13 -0300

    Modify read method

--- app/services/extractor.rb
+++ app/services/extractor.rb
@@ -1,17 +1,19 @@
 require 'csv'
 
 class Extractor
-  attr_reader :file
+  attr_reader :document
 
-  def initialize(file)
-    @file = file
+  def initialize(document)
+    @document = document
   end
 
   def read
-    CSV.foreach(file.tempfile, col_sep: ';', liberal_parsing: true) do |row|
-      Builder::Expense.new(
-        row.map { _1.gsub('"', '') }
-      ).save
+    document.blob.open do |tempfile|
+      CSV.foreach(tempfile, col_sep: ';', liberal_parsing: true) do |row|
+        Builder::Expense.new(
+          row.map { _1.gsub('"', '') }
+        ).save
+      end
     end
   end
 end
